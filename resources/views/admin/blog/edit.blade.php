da325fd5a6b9eb127c13a2ef3e635f2d97156f3d
Michael Joyner <joyner.michael@gmail.com>
2016-03-26 08:55:26 +0800

    tinymce config for blog edit pages

--- resources/views/admin/blog/edit.blade.php
+++ resources/views/admin/blog/edit.blade.php
@@ -18,7 +18,14 @@
     <script src="{{ asset('tinymce/tinymce.min.js') }}"></script>
     <script>
         tinymce.init({
-            selector: "#body-area"
+            selector: "#body-area",
+            plugins: ['link', 'paste', 'fullscreen'],
+            menubar: false,
+            toolbar: 'undo redo | styleselect | bold italic | link bullist numlist | fullscreen',
+            paste_data_images: false,
+            paste_as_text: true,
+            height: 500,
+            content_style: "body {font-size: 16px; max-width: 800px; margin: 0 auto;} * {font-size: 16px;}"
         });
     </script>
 @endsection
